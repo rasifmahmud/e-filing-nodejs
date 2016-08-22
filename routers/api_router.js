ca22feb577f2097a99cc2cd8454631244549a9f1
rasifmahmud <rasifmahmud16@gmail.com>
2016-08-22 18:00:20 +0600

    very vulnerable situation

--- routers/api_router.js
+++ routers/api_router.js
@@ -3,13 +3,24 @@
  */
 var express = require('express');
 var rfq = require("../models/RFQ");
-var user = require("../models/users");
+var rfqdetails = require("../models/RFQ_info");
 var router = express.Router();
 module.exports = router;
 
 router.route('/data')
     .get(function (req, res) {
-        res.json(req.user);
+        rfq.getFullRFQListbyUsername(req.user.username,function (err, result) {
+            if(err) return console.log(err);
+            console.log(result[0]._id);
+            rfqdetails.getRFQInfobyID(result._id,function (err2, result2) {
+                console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+                console.log(result._id);
+                console.log(result2);
+                res.json({user:req.user , rfq_list:result, rfq_info:result2});
+            });
+
+        });
+
     })
     .post(function (req, res) {
         res.sendStatus(200);
