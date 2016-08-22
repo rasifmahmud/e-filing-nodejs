8c2c8c668005d148dfc208d0b1c103e971565cca
rasifmahmud <rasifmahmud16@gmail.com>
2016-08-22 18:18:48 +0600

    razon:3 new rfq routes added

--- routers/api_router.js
+++ routers/api_router.js
@@ -9,16 +9,9 @@ module.exports = router;
 
 router.route('/data')
     .get(function (req, res) {
-        rfq.getFullRFQListbyUsername(req.user.username,function (err, result) {
-            if(err) return console.log(err);
-            console.log(result[0]._id);
-            rfqdetails.getRFQInfobyID(result._id,function (err2, result2) {
-                console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-                console.log(result._id);
-                console.log(result2);
-                res.json({user:req.user , rfq_list:result, rfq_info:result2});
-            });
-
+        rfq.getFullRFQListbyUsername(req.user.username, function (err, result) {
+            if (err) return console.log(err);
+            res.json({user: req.user, rfq_list: result});
         });
 
     })
