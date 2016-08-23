e760e918a6da07db90cc2eb3d50cf74c53ae277f
Ferdous Al Imran <imran05252000@gmail.com>
2016-08-23 12:22:55 +0600

    imran : router alpo changed

--- routers/dashboard_router.js
+++ routers/dashboard_router.js
@@ -15,17 +15,20 @@ module.exports = router;
 // routing to the dashboard
 router.route('/')
     .get(function (req, res) {
-        res.render('bn_BD/dashboard');
+        // res.render('bn_BD/dashboard');
+        res.render('bn_BD/dashboard',{ main_content:"default" });
     });
 
 router.route('/rfq_request')
     .get(function (req, res) {
-        res.render('bn_BD/RFQ_Request');
+        // res.render('bn_BD/RFQ_Request');
+        res.render('bn_BD/dashboard',{ main_content:"rfq_request" });
     });
 
 router.route('/rfq_running')
     .get(function (req, res) {
-        res.render('bn_BD/RFQ_Running');
+       // res.render('bn_BD/RFQ_Running');
+        res.render('bn_BD/dashboard',{ main_content:"rfq_running" });
     });
 
 router.route('/rfq_list')
