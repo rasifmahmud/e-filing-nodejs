d82f558a72267cb3cfd048b001eeea920bd98b80
Ferdous Al Imran <imran05252000@gmail.com>
2016-09-05 02:33:14 +0600

    roll back

--- public/js/RFQ_details.js
+++ public/js/RFQ_details.js
@@ -6,9 +6,6 @@ $(document).ready(function () {
 
     "use strict";
 
-
-    "use strict";
-
     // $("#main-content").html("<h1> thtihththtgthtihtihtthgth </h1>");
     var myDate = new Date().toString().slice(0, 21);
     document.getElementById("date").innerHTML = myDate;
@@ -16,14 +13,47 @@ $(document).ready(function () {
 
     var index_step = 7;
 
-    function show_detail(i) {
-        index_step = i;
-    }
 
 
     for (var i= index_step+1 ;i<=16;i++){
         document.getElementById('step' + i + '-panel').style.display = 'none';
     }
+// initially all the panels from 2-16 are hidden
+//     document.getElementById('step2-panel').style.display = 'none';
+//     document.getElementById('step3-panel').style.display = 'none';
+//     document.getElementById('step4-panel').style.display = 'none';
+//     document.getElementById('step5-panel').style.display = 'none';
+//     document.getElementById('step6-panel').style.display = 'none';
+//     document.getElementById('step7-panel').style.display = 'none';
+//     document.getElementById('step8-panel').style.display = 'none';
+//     document.getElementById('step9-panel').style.display = 'none';
+//     document.getElementById('step10-panel').style.display = 'none';
+//     document.getElementById('step11-panel').style.display = 'none';
+
+
+// declared the global variables
+// to keep track of the state of each step
+//     var step2_complete = false;
+//     var step3_complete = false;
+//     var step4_complete = false;
+//     var step5_complete = false;
+//     var step6_complete = false;
+//     var step7_complete = false;
+//     var step8_complete = false;
+//     var step9_complete = false;
+//     var step10_complete = false;
+//     var step11_complete = false;
+//
+//     if (step2_complete === true)  document.getElementById('step3-panel').style.display = 'block';
+//     if (step3_complete === true)  document.getElementById('step4-panel').style.display = 'block';
+//     if (step4_complete === true)  document.getElementById('step5-panel').style.display = 'block';
+//     if (step5_complete === true)  document.getElementById('step6-panel').style.display = 'block';
+//     if (step6_complete === true)  document.getElementById('step7-panel').style.display = 'block';
+//     if (step7_complete === true)  document.getElementById('step8-panel').style.display = 'block';
+//     if (step8_complete === true)  document.getElementById('step9-panel').style.display = 'block';
+//     if (step9_complete === true)  document.getElementById('step10-panel').style.display = 'block';
+//     if (step10_complete === true)  document.getElementById('step11-panel').style.display = 'block';
+//
 
     // step 1  er jonno necessary data start
     var rfq_heading = "";
@@ -74,17 +104,17 @@ $(document).ready(function () {
 
         }
 
-        // $("#rfq-heading").text(rfq_heading.slice(0, -2));
+        $("#rfq-heading").text(rfq_heading.slice(0, -2));
 
-        // $.ajax({
-        //     type: "POST",
-        //     url: "/api/upload",
-        //     data: JSON.stringify(jsonArray),
-        //     contentType: "application/json"
-        //
-        // }).done(function () {
-        //     console.log("Message sent successfully");
-        // });
+        $.ajax({
+            type: "POST",
+            url: "/api/upload",
+            data: JSON.stringify(jsonArray),
+            contentType: "application/json"
+
+        }).done(function () {
+            console.log("Message sent successfully");
+        });
 
 
     });
