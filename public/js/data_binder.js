bf24ddd83a1afe11e2d0f668deeb9a886d25a40b
Ferdous Al Imran <imran05252000@gmail.com>
2016-09-05 01:12:11 +0600

    imran: hall e kaj

--- public/js/data_binder.js
+++ public/js/data_binder.js
@@ -4,6 +4,9 @@ $.ajax({
     url: "/api/data",
 }).done(function (data) {
     special_data = data;
+    RFQ_index = document.URL;
+    RFQ_index = RFQ_index.slice(0,-1);
+    RFQ_index = RFQ_index.substr(RFQ_index.lastIndexOf('/')+1);
     $.holdReady(false);
 });
 
